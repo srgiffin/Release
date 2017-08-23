666692cdcfbdbb21ab2d0e85e127e0a685ec7c8e
Stephen R. Giffin <DEMO\srgiffin>
2017-08-23 18:39:11 +0000

    Adding example
    git-tfs-id: [http://demo17:8080/tfs/democollection]$/DEQDemo/Main;C11

---
+++ SpecFlowValidation-Solution/SpeclFlowUnitTest/CalculatorSteps.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Example;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TechTalk.SpecFlow;
+
+namespace SpeclFlowUnitTest
+{
+    [Binding]
+    public class CalculatorSteps
+    {
+        private int TestResult { get; set; }
+        private Example.Calculator _calculator = new Calculator();
+
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int number)
+        {
+            _calculator.FirstNumber = number;
+        }
+        
+        [Given(@"I have also entered (.*) into the calculator")]
+        public void GivenIHaveAlsoEnteredIntoTheCalculator(int number)
+        {
+            _calculator.SecondNumber = number;
+        }
+        
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            TestResult = _calculator.Add();
+        }
+        
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int expectedResult)
+        {
+            Assert.AreEqual(expectedResult, TestResult);
+        }
+    }
+}
---
+++ SpecFlowValidation-Solution/SpeclFlowUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SpeclFlowUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SpeclFlowUnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8d915e52-5450-4950-8f2d-60fbd40fc338")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
